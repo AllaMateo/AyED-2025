0861c930ac1bff09c523ac7a336ce745fe2972de
AllaMateo <allamandrimateo@gmail.com>
2025-08-28 18:07:35 -0300

    Add files via upload

---
+++ semana4/src/Recinto.cpp
@@ -0,0 +1,61 @@
+#include "Recinto.h"
+#include <iostream>
+
+Recinto::Recinto(std::string nombre, int nSensores){
+    this->nombre = nombre;
+    this->nSensores = nSensores;
+    this->sensores = new Sensor*[nSensores];
+    for(int i=0; i<nSensores; i++) {
+        this->sensores[i] = nullptr;
+    }
+}
+
+Recinto::~Recinto(){
+    delete[] sensores;
+}
+
+std::string Recinto::getNombre(){
+    return nombre;
+}
+
+int Recinto::getCantSensores(){
+    return nSensores;
+}
+
+void Recinto::setNombre(std::string nombreNuevo){
+    this->nombre = nombreNuevo;
+}
+
+// ------------------ RECURSIVOS ------------------
+
+float Recinto::_suma(Sensor** v, int n, std::string unidad){
+    //TODO
+    if (n==this->nSensores) return 0.0;
+    else {
+        if (v[n]!=nullptr && [n]->getUnidad()==unidad) return _suma(v,n+1,unidad) + v[n]->getValor());
+        else return _suma(v, n+1, unidad);        
+        }
+    
+}
+
+int Recinto::_maximo(Sensor** v, int n, std::string unidad, int mejorPos){
+    //TODO
+    if (n==this->nSensores) return 0;
+    else {
+        if (v[n]!=nullptr && [n]->getUnidad()==unidad) {
+            if (v[mejorPos]->getValor() < v[n]->getValor()) 
+                mejorPos = n;
+            return _maximo(v,n+1,unidad,mejorPos) + v[n]->getValor();}
+        else 
+            return _maximo(v, n+1, unidad, mejorPos);
+        }
+}
+
+int Recinto::_buscar(Sensor** v, int n, const std::string& key){
+    //TODO
+}
+
+Sensor* Recinto::_getSensor(Sensor** v, int n, int posBuscada){
+    //TODO
+    
+}
